886c1326d5f2703614f383c0e5b93473ae43e350
Sreehas Dommata <sreehasdommata98@gmail.com>
2020-04-18 20:00:25 +0530

    sd

---
+++ src/pages/404.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Search from "../search/search";
+
+const NotFoundPage = () => (
+  <div>
+    <h1>Nothing Found</h1>
+    /*<p>You just hit a route that doesn&#39;t exist... the sadness.</p>*/
+	<p>Sorry, but nothing matched your search terms. Please try again with some different keywords. If you're still unsuccessful, you can always request an article or write one yourself.</p>
+  </div>
+);
+
+
+   <Search />
+
+export default NotFoundPage;
